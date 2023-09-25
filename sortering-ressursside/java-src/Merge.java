fad828ac909111e4bb5ba7b3e7ee4afad10b4509
Sebastian Hareide <84544523+harepus@users.noreply.github.com>
2023-09-25 20:11:03 +0200

    new try

---
+++ sortering-ressursside/java-src/Merge.java
@@ -0,0 +1,19 @@
+class Merge extends Sorter {
+
+    void sort() {
+
+        // Do merge sort here. Use the Sorter's comparison- and swap
+        // methods for automatically counting the swaps and comparisons.
+        //
+        // The object has a field A and n, where A is to be sorted and n is its
+        // length.
+        //
+        // Alternatively, you can "manually" increment the fields `comparisons'
+        // and `swaps' at your own leisure.
+
+    }
+
+    String algorithmName() {
+        return "merge";
+    }
+}
